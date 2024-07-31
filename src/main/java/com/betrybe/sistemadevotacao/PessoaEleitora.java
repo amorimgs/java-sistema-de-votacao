88cea10a05e2d0a74f1b7b1340631225542d911e
Gabriel Amorim <ricksamann@gmail.com>
2024-07-31 08:41:48 -0300

    feat: Implementado classe PessoaEleitora

--- src/main/java/com/betrybe/sistemadevotacao/PessoaEleitora.java
+++ src/main/java/com/betrybe/sistemadevotacao/PessoaEleitora.java
@@ -1,5 +1,20 @@
 package com.betrybe.sistemadevotacao;
 
-public class PessoaEleitora{
+public class PessoaEleitora extends Pessoa {
+
+  private String cpf;
+
+  public PessoaEleitora(String nome, String cpf) {
+    this.cpf = cpf;
+    this.nome = nome;
+  }
+
+  public String getCpf() {
+    return cpf;
+  }
+
+  public void setCpf(String cpf) {
+    this.cpf = cpf;
+  }
 
 }
