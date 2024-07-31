fd1db56d1cc6b2acc7c1a3db5cc0d79087aa5e1d
Gabriel Amorim <ricksamann@gmail.com>
2024-07-31 10:29:03 -0300

    feat: Adicionado javadoc

--- src/main/java/com/betrybe/sistemadevotacao/Principal.java
+++ src/main/java/com/betrybe/sistemadevotacao/Principal.java
@@ -2,8 +2,16 @@ package com.betrybe.sistemadevotacao;
 
 import java.util.Scanner;
 
+/**
+ * The type Principal.
+ */
 public class Principal {
 
+  /**
+   * The entry point of application.
+   *
+   * @param args the input arguments
+   */
   public static void main(String[] args) {
     // TODO Auto-generated method stub
     Scanner scanner = new Scanner(System.in);
