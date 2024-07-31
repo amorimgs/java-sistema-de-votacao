7636cb6d1b3ca9683691be5f3ffee5c785c735fe
Gabriel Amorim <ricksamann@gmail.com>
2024-07-31 09:08:34 -0300

    feat: Implementado metodos de cadastro da classe GerenciamentoVotacao

--- src/main/java/com/betrybe/sistemadevotacao/GerenciamentoVotacao.java
+++ src/main/java/com/betrybe/sistemadevotacao/GerenciamentoVotacao.java
@@ -4,19 +4,33 @@ import java.util.ArrayList;
 
 public class GerenciamentoVotacao implements GerenciamentoVotacaoInterface {
 
-  private ArrayList<PessoaCandidata> pessoasCandidatas;
-  private ArrayList<PessoaEleitora> pessoasEleitoras;
-  private ArrayList<String> cpfsComputados;
+  private ArrayList<PessoaCandidata> pessoasCandidatas = new ArrayList<PessoaCandidata>();
+  private ArrayList<PessoaEleitora> pessoasEleitoras = new ArrayList<PessoaEleitora>();
+  private ArrayList<String> cpfsComputados = new ArrayList<String>();
 
 
   @Override
   public void cadastrarPessoaCandidata(String nome, int numero) {
-
+    PessoaCandidata candidataCadastrada = pessoasCandidatas.stream()
+        .filter(p -> numero == p.getNumero()).findAny().orElse(null);
+    if (candidataCadastrada != null) {
+      System.out.println("Número da pessoa candidata já utilizado!");
+    } else {
+      PessoaCandidata pessoaCandidata = new PessoaCandidata(nome, numero);
+      pessoasCandidatas.add(pessoaCandidata);
+    }
   }
 
   @Override
   public void cadastrarPessoaEleitora(String nome, String cpf) {
-
+    PessoaEleitora eleitoraCadastrada = pessoasEleitoras.stream()
+        .filter(p -> cpf.contains(p.getCpf())).findAny().orElse(null);
+    if (eleitoraCadastrada != null) {
+      System.out.println("Pessoa eleitora já cadastrada!");
+    } else {
+      PessoaEleitora pessoaEleitora = new PessoaEleitora(nome, cpf);
+      pessoasEleitoras.add(pessoaEleitora);
+    }
   }
 
   @Override
