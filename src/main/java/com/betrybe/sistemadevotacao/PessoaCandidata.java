4bb669f301bdda9db6c274e3b8ff0ed0f4281401
Gabriel Amorim <ricksamann@gmail.com>
2024-07-30 23:59:39 -0300

    feat: Implementado classe PessoaCandidata

--- src/main/java/com/betrybe/sistemadevotacao/PessoaCandidata.java
+++ src/main/java/com/betrybe/sistemadevotacao/PessoaCandidata.java
@@ -1,5 +1,29 @@
 package com.betrybe.sistemadevotacao;
 
-public class PessoaCandidata {
+public class PessoaCandidata extends Pessoa {
 
+  private int numero;
+  private int votos;
+
+  public PessoaCandidata(String nome, int numero) {
+    this.numero = numero;
+    this.nome = nome;
+    this.votos = 0;
+  }
+
+  public void receberVoto() {
+    this.votos += 1;
+  }
+
+  public int getNumero() {
+    return numero;
+  }
+
+  public int getVotos() {
+    return votos;
+  }
+
+  public void setNumero(int numero) {
+    this.numero = numero;
+  }
 }
